06e86f7d70b6c78edc968b9e8957dfc941ccc27c
Venkatakrishnan <venkatakrishnan434@gmail.com>
2022-09-29 18:51:52 +0530

    Student file

--- name_marks.rb
+++ name_marks.rb
@@ -1,14 +1,24 @@
- hash = {}
-for i in (1..10)
+hash = {}
+10.times{
     puts "Enter Name"
     name = gets.chomp
     puts "Enter marks"
     marks = gets.chomp
 
     hash[name] = marks
+
+}
+
+puts "Find the student marks"
+
+students = gets.chomp
+
+
+if value = hash[students]
+    puts " #{students} mark is #{value} out of 100"
+else
+    puts "Fail"
 end
 
-puts "#{hash}"
-
-
-
+ str = hash.sort_by{|k, v|  [-v, k]}
+ p str.reverse
